c5126402e455847fe6b0bd475c6fe7304ae58f79
rpiguy <admin@rpiguy.us>
2014-05-03 21:14:27 +0200

    Blink-Program

--- Blink-Program.py
+++ Blink-Program.py
@@ -5,8 +5,12 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT) # Setting up GPIO pin 7
 GPIO.output(7,0)
 
-while True: # Creating a never ending loop except if you press CTRL + C
-    GPIO.output(7,1) # Turning it on
-    time.sleep(1) # Time Delay between on and off
-    GPIO.output(7,0) # Turning it off
-    time.sleep(1)
+try:
+
+    while True: # Creating a never ending loop except if you press CTRL + C
+        GPIO.output(7,1) # Turning it on
+        time.sleep(1) # Time Delay between on and off
+        GPIO.output(7,0) # Turning it off
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
